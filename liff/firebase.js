5b1a368490f335e86037b08cc134995b5a99576b
Oscar Liu <oscar@symptomtrace.com>
2025-09-05 19:34:17 +0800

    save current status@Sep. 5 19:34

--- liff/firebase.js
+++ liff/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+// Added the import for Firebase Storage
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export { app, db };
+// Initialize Firebase Storage and get a reference to the service
+const storage = getStorage(app);
+
+export { app, db, storage };
